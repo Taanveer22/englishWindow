ccfafd19812ee914244aaba0d5fc1809777a4c8b
Taanveer22 <taanveer469@gmail.com>
2026-02-13 21:01:30 +0600

    alhamdulliah lesson category loaded from api and showed and in the ui

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log('connected');
+// console.log("connected");
+
+const loadLessons = () => {
+  fetch(`https://openapi.programming-hero.com/api/levels/all`)
+    .then((res) => res.json())
+    .then((data) => displayLessons(data.data));
+};
+
+const displayLessons = (categories) => {
+  // 1. select the container and make innerHTML empty
+  const lessonsContainer = document.getElementById("lessons-container");
+  // console.log(lessonsContainer);
+  lessonsContainer.innerHTML = "";
+  // 2. loop through every lesson
+  for (oneCategory of categories) {
+    // console.log(oneCategory);
+    // 3. create div element to keep data
+    const div = document.createElement("div");
+    div.innerHTML = `
+                <button class="flex items-center gap-1 btn btn-sm btn-outline btn-primary">
+                  <i class="fa-solid fa-book-open"></i>
+                  <p>lesson - ${oneCategory.level_no}</p>
+                </button>
+              `;
+    // 4. append the container child elements
+    lessonsContainer.appendChild(div);
+  }
+
+  console.log(categories);
+};
+
+loadLessons();
